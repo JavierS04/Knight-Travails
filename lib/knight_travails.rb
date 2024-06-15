389a3ef24eae526e5f711d4e6bea45f47a7c019e
JavierS04 <javiersemper509@gmail.com>
2024-06-15 02:11:19 +0100

    finished creating knight travails code is able to calculate how many steps needed to go from one position to another is room for some optimisation, something i will do in the future

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -23,8 +23,11 @@ class Node
             possible_moves << [new_row, new_col]
           end
         end
-    
         possible_moves
+      end
+
+      def get_pm
+        return @possible_moves
       end
 end
 
@@ -45,11 +48,11 @@ class Board
         grid
     end
     
-    def valid_move(value, move)
-        node = find_node(value)
-        puts node.possible_moves.inspect
-        node.possible_moves.include?(move) ? "valid move" : "move not valid"
-    end 
+    # def valid_move(value, move)
+    #     node = find_node(value)
+    #     puts node.possible_moves.inspect
+    #     node.possible_moves.include?(move) ? "valid move" : "move not valid"
+    # end 
 
     def find_node(value)
         (0...8).each do |row|
@@ -60,10 +63,52 @@ class Board
         nil # Return nil if the node is not found
     end
     
-    
+    def move (start_point, end_point)
+        return p "invalid position entered" if !find_node(start_point) || !find_node(end_point)
+
+        queue = [find_node(start_point)]
+        parents = []
+        found = false
+
+        while found == false
+          node = queue.shift
+
+          if node == nil
+          elsif node.value == end_point
+            found = true
+          else 
+              parents << node unless parents.include?(node)
+          end
+
+          node.get_pm.each do |possible|
+            queue << find_node(possible) unless queue.include?(possible)
+          end
+        end
+
+        correct_list = []
+        current = end_point
+        until found == false
+          previous = parents.pop
+                    
+          if previous == nil
+            found = false
+            break
+          end
+
+          if previous.possible_moves.include?(current)
+            correct_list << previous.value
+            current = previous.value
+          end
+        end
+
+        correct_list = correct_list.reverse
+        correct_list << end_point
+        puts "You made it in #{correct_list.length - 1}. Here's the path: "
+        puts correct_list.inspect
+    end
 end
 
 
 a = Board.new()
-a.move([3,3],[4,1])
+a.move([-3,-3],[3,3])
 
